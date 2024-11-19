6ac83df9377fff0c984dc0928966fd7cc8c80bd1
AndreSurnam <andre.tolentino.personal@gmail.com>
2024-11-19 17:23:34 -0500

    Navbar complete (maybe)

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const menu_toggle = document.querySelector(".menu-toggle");
-const sidebar = document.querySelector(".sidebar");
+const collapse = document.querySelector(".collapse-bar");
 
 menu_toggle.addEventListener("click", () => {
   menu_toggle.classList.toggle("is-active");
-  sidebar.classList.toggle("is-active");
+  collapse.classList.toggle("is-active");
 });
