20b7d3c0b0c852ad549c0227f7e29fc9d14ccbde
AndreSurnam <andre.tolentino.personal@gmail.com>
2024-11-27 21:59:26 -0500

    Can 99% say this, About page is done. Added a few things in collapse bar, of which are minor changed. Will try and find out how to blur everything except the navbar, menu toggle and collapsed bar when collapse bar is active.

--- script.js
+++ script.js
@@ -1,12 +1,20 @@
 const menuToggle = document.querySelector('.menu-toggle');
 const collapseBar = document.querySelector('.collapse-bar');
+const navbar = document.getElementById('navbar');
 
 menuToggle.addEventListener('click', function () {
   collapseBar.classList.toggle('is-active');
   menuToggle.classList.toggle('is-active');
   if (collapseBar.classList.contains('is-active')) {
     collapseBar.classList.add('is-sticky');
+    navbar.classList.add('no-shadow');
+    document.body.classList.add('blur-active');
+    
   } else {
     collapseBar.classList.remove('is-sticky');
+    navbar.classList.remove('no-shadow');
+    document.body.classList.remove('blur-active');
   }
 });
+
+
